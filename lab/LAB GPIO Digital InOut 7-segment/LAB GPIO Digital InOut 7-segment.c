154595b26ba739be79194fb82c3177e8595f64d7
henny041520-commits <henny041520@gmail.com>
2025-09-23 03:37:37 +0900

    d

---
+++ lab/LAB GPIO Digital InOut 7-segment/LAB GPIO Digital InOut 7-segment.c
@@ -0,0 +1,35 @@
+#include "stm32f4xx.h"
+#include "ecGPIO2.h"
+#include "ecRCC2.h"
+#include "stdio.h"
+
+// Initialize 7 DOUT pins for 7 segment leds
+void seven_seg_FND_init(void); 
+int readButtonRising(PinName_t pinName);
+void seven_seg_FND_display(uint8_t  num, uint8_t select);
+void setup(void){
+    RCC_HSI_init();
+    seven_seg_FND_init(); 
+};
+int main(void) {
+    setup();
+    
+    uint8_t selectFND=0;  
+    uint8_t count = 0;
+    while (1) {
+        if(readButtonRising(PA_4)){
+        count++;
+}
+        if(count==10)
+        {
+            count=0;
+        }
+        seven_seg_FND_display(count,selectFND);
+    }
+}
+
+
+// Initialize DOUT pins for 7 segment leds
+
+
+
